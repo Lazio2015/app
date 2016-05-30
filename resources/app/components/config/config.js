a0eb9d9406d372efc5f9269f31bcedb5f8d74964
lenur <lenur.m@wecandevelopit.com>
2016-05-30 18:46:26 +0300

    refactoring

--- resources/app/components/config/config.js
+++ resources/app/components/config/config.js
@@ -5,43 +5,7 @@ function AppConfig($stateProvider, $urlRouterProvider) {
             templateUrl: 'templates/words/words.html',
             controller: 'WordsCtrl',
             controllerAs: 'words'
-        })
+        });
 
-
-
-
-        .state('tab', {
-            url: '/tab',
-            abstract: true,
-            templateUrl: 'templates/controllers/tabs.html'
-        })
-        .state('tab.city', {
-            url: '/city',
-            views: {
-                'tab-city': {
-                    templateUrl: 'templates/controllers/tab-city.html',
-                    controller: 'CityCtrl'
-                }
-            }
-        })
-        .state('tab.city-detail', {
-            url: '/city/:id',
-            views: {
-                'tab-city': {
-                    templateUrl: 'templates/controllers/city-detail.html',
-                    controller: 'CityDetailCtrl'
-                }
-            }
-        })
-        .state('tab.example', {
-            url: '/example',
-            views: {
-                'example': {
-                    templateUrl: 'templates/controllers/example.html',
-                    controller: 'ExampleCtrl',
-                    controllerAs: 'exCtrl'
-                }
-            }
-        })
     $urlRouterProvider.otherwise('/words');
 }
